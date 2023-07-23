fe8057f5ec75db360d22555ed0e85c1047e1fabd
Barry <87644022+fastcode555@users.noreply.github.com>
2023-07-23 18:04:35 +0800

    增加一个计数器的功能

---
+++ src/Counter.vue
@@ -0,0 +1,14 @@
+<script setup>
+import {reactive} from "vue";
+
+const state = reactive({count: 0})
+
+const setCount = () => {
+  state.count++
+}
+</script>
+<template>
+  <div>
+    <button @click="setCount">{{ state.count}}</button>
+  </div>
+</template>
